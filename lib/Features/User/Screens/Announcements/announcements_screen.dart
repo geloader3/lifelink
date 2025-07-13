9c18ad629123fa5aa5eaa9e1137432c34e4fecfd
geloader3 <geloader3@gmail.com>
2025-07-14 03:12:35 +0800

    Fix DateFormat import error

--- lib/Features/User/Screens/Announcements/announcements_screen.dart
+++ lib/Features/User/Screens/Announcements/announcements_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
-import '../../../Common Widgets/constants.dart';
+import '../../../../Common Widgets/constants.dart';
 import '../../Controllers/announcement_controller.dart';
 
 class AnnouncementsScreen extends StatefulWidget {
